ea2c582fd8b65be88e347013fd299c1055bd0449
Sebosun <sebos@protonmail.com>
2021-02-26 01:35:30 +0100

    displays different colors based on a field

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, {useState} from "react";
-import './App.css';
-import './factories/shipFactory'
-import Gameboard from "./factories/Gameboard"
+import React, { useState } from "react";
+import "./App.css";
+import "./factories/shipFactory";
+import Gameboard from "./factories/Gameboard";
 import ShipFactory from "./factories/shipFactory.js";
 import Player from "./factories/Player";
-import DrawBoard from "./components/DrawBoard"
+import DrawBoard from "./components/DrawBoard";
 
 function App() {
+  let [PlayerBoard, setPlayerBoard] = useState(Gameboard(10, 10));
+  let [NPCBoard, setNPCBoard] = useState(Gameboard(10, 10));
 
-  let [PlayerGameboard, setPlayerBoard] = useState(Gameboard(10,10));
-  let [NPCGameboard, setNPCGameboard] = useState(Gameboard(10,10));
- 
-  NPCGameboard.receiveAttack(1,1);
-  console.log(NPCGameboard.board);
-
-
-
+  NPCBoard.receiveAttack(1, 1);
+  NPCBoard.receiveAttack(1, 7);
+  NPCBoard.receiveAttack(1, 5);
+  NPCBoard.receiveAttack(4, 1);
   // The idea is that board will be forwarded to components, once the state changes, the board will redraw
+
   return (
-    <div className="App">
-      <DrawBoard 
-        board={PlayerGameboard.board}
-      />
+    <div className="main">
+      <div className="npccontainer">
+        <p>NPC</p>
+        <DrawBoard board={NPCBoard.board} />
+      </div>
+      <div className="playercontainer">
+        <p>Player</p>
+        <DrawBoard board={PlayerBoard.board} />
+      </div>
     </div>
   );
 }
